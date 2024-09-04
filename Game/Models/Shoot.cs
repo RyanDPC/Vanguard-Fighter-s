ef19c49429de65bcb1644d26b4ce1547cf604377
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2024-09-04 13:37:09 +0200

    ...

---
+++ Game/Models/Shoot.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace My2DGame
+{
+    public class Shoot
+    {
+        public Vector2 Position { get; private set; }
+        public Vector2 Velocity { get; private set; }
+        private Texture2D texture;
+
+        public Shoot(Texture2D texture, Vector2 position, Vector2 velocity)
+        {
+            this.texture = texture;
+            this.Position = position;
+            this.Velocity = velocity;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Position += Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, Color.White);
+        }
+    }
+}
