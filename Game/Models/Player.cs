68015eb12d50e684348e0acacccfa828af46d082
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2024-09-06 15:02:32 +0200

    Update Code player

--- Game/Models/Player.cs
+++ Game/Models/Player.cs
@@ -1,16 +1,48 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
 
 namespace Game.Content.Models
 {
     internal class Player
     {
         private Texture2D playerTexture;
-        private Vector2 playerPosition;
+        public Vector2 playerPosition;
+        public Rectangle hitbox;
+
+        public Player(Texture2D texture, Vector2 initialPosition)
+        {
+            playerTexture = texture;
+            playerPosition = initialPosition;
+            UpdateHitbox();
+        }
+
+        // Call this function whenever the player moves to update the hitbox
+        private void UpdateHitbox()
+        {
+            hitbox = new Rectangle(
+                (int)playerPosition.X,
+                (int)playerPosition.Y,
+                playerTexture.Width,
+                playerTexture.Height
+            );
+        }
+
+        public void Move(Vector2 newPosition)
+        {
+            playerPosition = newPosition;
+            UpdateHitbox();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(playerTexture, playerPosition, Color.White);
+        }
     }
 }
