ce2364ec5f541bc0df84d0c12e0d8199be931881
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2024-09-07 01:23:33 +0200

    Map

---
+++ Game/Content/Map/TitleMapAssetsCastle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TitleMapAssetsCastle" tilewidth="32" tileheight="32" tilecount="690" columns="30">
+ <image source="../../../../../../Downloads/Castle Plataformer Tileset/Sprite Sheet 1.png" width="964" height="746"/>
+</tileset>
