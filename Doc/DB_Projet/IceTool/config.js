886e2d995067217ed49c87a5d6a3b502cb765163
RyanDePina <156648185+RyanDPC@users.noreply.github.com>
2024-10-09 13:11:13 +0200

    Add(Content)

---
+++ Doc/DB_Projet/IceTool/config.js
@@ -0,0 +1,6 @@
+const iceScrumToken = "bc1950469c754b76acd556fea80d8c11";
+// 
+const projects = [
+  { projectKey: "SPACEINVA", cartouche: {} },
+];
+const API_base_url = "https://etml.icescrum.com/ws/project/";
---
+++ Doc/POO_Projet/IceTools-Shoot-Me/config.js
@@ -0,0 +1,7 @@
+const iceScrumToken = "bc1950469c754b76acd556fea80d8c11";
+// 
+const projects = [
+  { projectKey: "PROGVANGUA", cartouche: {} },
+
+];
+const API_base_url = "https://etml.icescrum.com/ws/project/";
